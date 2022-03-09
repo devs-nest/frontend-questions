efb77ce2ecf5bf99c16830e17994746f8954b730
SooditK <sooditkumarabc@gmail.com>
2022-03-09 08:03:41 +0530

    feat: removed materialui button input

--- src/App.js
+++ src/App.js
@@ -1,143 +1,153 @@
-import { Button, TextField } from "@mui/material";
-import React, { useState, useEffect } from "react";
-import { BsCheck2All } from "react-icons/bs";
-import { FiTrash } from "react-icons/fi";
+import React, { useState, useEffect } from 'react'
+import { BsCheck2All } from 'react-icons/bs'
+import { FiTrash } from 'react-icons/fi'
 
 function App() {
-  const [value, setValue] = useState("");
-  const [todo, setTodo] = useState([]);
+  const [value, setValue] = useState('')
+  const [todo, setTodo] = useState([])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(value);
-    const data = await fetch("https://sherdil.herokuapp.com/todo", {
-      method: "POST",
+    e.preventDefault()
+    console.log(value)
+    const data = await fetch('https://sherdil.herokuapp.com/todo', {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         title: value,
         completed: false,
       }),
-    });
-    setValue("");
-    getAllTodos();
-  };
+    })
+    setValue('')
+    getAllTodos()
+  }
 
   const handleComplete = async (id) => {
     const data = await fetch(`https://sherdil.herokuapp.com/todo`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Allow-Control-Allow-Origin": "*",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Allow-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         completed: true,
         id: parseInt(id),
       }),
-    });
-    getAllTodos();
-  };
+    })
+    getAllTodos()
+  }
 
   const handleCompleteFalse = async (id) => {
     const data = await fetch(`https://sherdil.herokuapp.com/todo`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Allow-Control-Allow-Origin": "*",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Allow-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         completed: false,
         id: parseInt(id),
       }),
-    });
-    getAllTodos();
-  };
+    })
+    getAllTodos()
+  }
 
   const handleDelete = async (id) => {
     const data = await fetch(`https://sherdil.herokuapp.com/todo`, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Allow-Control-Allow-Origin": "*",
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Allow-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
         id: parseInt(id),
       }),
-    });
-    getAllTodos();
-  };
+    })
+    getAllTodos()
+  }
 
   const getAllTodos = async () => {
-    const response = await fetch("https://sherdil.herokuapp.com/todos");
-    const data = await response.json();
-    setTodo(data);
-  };
+    const response = await fetch('https://sherdil.herokuapp.com/todos')
+    const data = await response.json()
+    setTodo(data)
+  }
 
   useEffect(() => {
-    getAllTodos();
-  }, [value]);
+    getAllTodos()
+  }, [value])
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
       }}
     >
       <h1>Todo Application</h1>
-      <TextField
+      <input
         id="standard-basic"
         label="Write a Todo"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        style={{ maxWidth: "300px", width: "90vw" }}
+        style={{ maxWidth: '300px', width: '90vw' }}
       />
-      <Button onClick={handleSubmit}>Submit</Button>
+      <button
+        style={{
+          padding: '10px',
+          margin: '10px',
+          backgroundColor: '#3f51b5',
+          color: 'white',
+          borderRadius: '10px',
+        }}
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
       {/* <pre>{JSON.stringify(todo, null, 2)}</pre> */}
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "90vw",
-          fontSize: "2rem",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '90vw',
+          fontSize: '2rem',
         }}
       >
         {todo &&
           todo.map((todo) => (
             <div
               style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                margin: "10px",
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                margin: '10px',
               }}
             >
               <div>{todo.title}</div>
               <span
                 style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginLeft: "30px",
-                  marginRight: "30px",
-                  gap: "10px",
-                  cursor: "pointer",
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginLeft: '30px',
+                  marginRight: '30px',
+                  gap: '10px',
+                  cursor: 'pointer',
                 }}
               >
                 {!todo.completed && (
                   <span
                     onClick={() => handleComplete(todo.id)}
                     style={{
-                      display: "flex",
-                      color: "red",
+                      display: 'flex',
+                      color: 'red',
                     }}
                   >
                     <BsCheck2All />
@@ -147,8 +157,8 @@ function App() {
                   <span
                     onClick={() => handleCompleteFalse(todo.id)}
                     style={{
-                      display: "flex",
-                      color: "green",
+                      display: 'flex',
+                      color: 'green',
                     }}
                   >
                     <BsCheck2All />
@@ -157,8 +167,8 @@ function App() {
                 <span
                   onClick={() => handleDelete(todo.id)}
                   style={{
-                    display: "flex",
-                    color: "blue",
+                    display: 'flex',
+                    color: 'blue',
                   }}
                 >
                   <FiTrash />
@@ -168,7 +178,7 @@ function App() {
           ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
